708592d7f2d4a8467ccc5a0cf6aa073041bf76ff
Seme4ka <nadyasimachen@gmail.com>
2026-07-06 17:50:40 +0300

    game sheets

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { GameSelect } from "./routes/GameSelect.js";
 import { BattleshipPlaceholder } from "./routes/BattleshipPlaceholder.js";
 import { DicePlaceholder } from "./routes/DicePlaceholder.js";
+import { Battleship } from "./games/battleship/battleship.js";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<GameSelect />} />
-      <Route path="/battleship" element={<BattleshipPlaceholder />} />
+      <Route path="/battleship" element={<Battleship />} />
       <Route path="/dice" element={<DicePlaceholder />} />
     </Routes>
   );
---
+++ apps/web/src/games/battleship/battleship.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+type Ship = [id: number, cells: [], health: number];
+
+export function Battleship() {
+  const [isDefenseBoardDisabled, setIsDefenseBoardDisabled] = useState(true);
+
+  const [attackSheet, setAttackSheet] = useState(
+    Array.from({ length: 10 }, () => Array(10).fill(false)),
+  );
+  const [defenseSheet, setDefenseSheet] = useState(
+    Array.from({ length: 10 }, () => Array(10).fill(false)),
+  );
+  // attackSheet[row][col]
+  //   console.log(attackSheet);
+
+  function getAttackCellColor(marked: boolean, rowIndex: number, colIndex: number) {
+    if (!marked) return "bg-white";
+
+    return defenseSheet[rowIndex]?.[colIndex] ? "bg-amber-300" : "bg-blue-500";
+  }
+
+  function getDefenseCellColor(marked: boolean, rowIndex: number, colIndex: number) {
+    if (marked && attackSheet[rowIndex]?.[colIndex]) return "bg-red-300";
+    if (marked) return "bg-green-500";
+
+    if (!marked && attackSheet[rowIndex]?.[colIndex]) return "bg-blue-500";
+    if (!marked) return "bg-white";
+  }
+
+  return (
+    <>
+      {/* Attack */}
+      <div className="flex flex-row gap-5 justify-center">
+        <div className="flex flex-row">
+          <p>Attack</p>
+          {attackSheet.map((row, rowIndex) => (
+            <div className="flex flex-col">
+              {row.map((marked, colIndex) => (
+                <button
+                  key={`${rowIndex}-${colIndex}`}
+                  disabled={isDefenseBoardDisabled || marked}
+                  className={`w-8 h-8 border border-gray-500 ${getAttackCellColor(marked, rowIndex, colIndex)}`}
+                  onClick={() => {
+                    setAttackSheet((prev) =>
+                      prev.map((row, r) =>
+                        row.map((cell, c) => (r === rowIndex && c === colIndex ? !cell : cell)),
+                      ),
+                    );
+                  }}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+
+        {/* Deffence */}
+        <div className="flex flex-row">
+          <p>Deffence</p>
+          {defenseSheet.map((row, rowIndex) => (
+            <div className="flex flex-col">
+              {row.map((marked, colIndex) => (
+                <button
+                  key={`${rowIndex}-${colIndex}`}
+                  disabled={!isDefenseBoardDisabled || marked}
+                  className={`w-8 h-8 border border-gray-500 ${getDefenseCellColor(marked, rowIndex, colIndex)}`}
+                  onClick={() => {
+                    setDefenseSheet((prev) =>
+                      prev.map((row, r) =>
+                        row.map((cell, c) => (r === rowIndex && c === colIndex ? !cell : cell)),
+                      ),
+                    );
+                  }}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+        <button
+          className="border pointer bg-amber-300"
+          onClick={() => setIsDefenseBoardDisabled(false)}
+        >
+          STOP
+        </button>
+      </div>
+    </>
+  );
+}
