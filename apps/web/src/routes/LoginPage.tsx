88957d8c59096eaeef5c78b7ecfdec309f15a97b
TetinaM <tetianamartynova05@gmail.com>
2026-09-11 02:31:40 +0200

    fonts, colors

--- apps/web/src/routes/LoginPage.tsx
+++ apps/web/src/routes/LoginPage.tsx
@@ -9,49 +9,53 @@ export function LoginPage () {
 
 
  return (
-    <body className="mx-auto flex max-w-md flex-col p-2">
+    <body className="mx-auto flex flex-col p-2 bg-black">
 
-        <div className="mx-auto flex max-w-md flex-col p-2" >
+        <header className="mx-auto w-130 flex-col pb-5" >
             <img src={logo} alt="logo" />
-        </div>
-        <div className="mx-auto flex max-w-md flex-col p-2">
-            <h1 className="uppercase text-green mx-auto flex font-display "> &gt; User authentication</h1>
-            <p>Access your global save progress and arcade rank.</p>
+        </header>
+
+        
+        <div  className='mx-auto max-w-md' >
+        <div className="  p-4">
+            <h1 className="uppercase text-green font-display text-lg "> &gt;User authentication</h1>
+            <p className='text-sm text-grey'>Access your global save progress and arcade rank.</p>
         
         </div>
       
            <form action="">
-            <label htmlFor="ussername">&gt; Username: </label>
-            <input className="block w-full border border-grey" placeholder="&gt; " name="ussername" type="text" />
-            <label htmlFor="password">&gt; Password:</label>
-            <input className="block w-full border border-grey " placeholder="&gt; " name="password" type="text" />
+            <label className='uppercase text-grey font-display' htmlFor="ussername">&gt; Username: </label>
+            <input className="block w-full border border-grey py-2 uppercase text-grey" placeholder="&gt; pixel_ " name="ussername" type="text" />
+            <label className='uppercase text-grey font-display  ' htmlFor="password">&gt; Password:</label>
+            <input className="block w-full border border-grey py-2 uppercase text-grey " placeholder="&gt; enter secure pass" name="password" type="text" />
 
 
-            <p className="uppercase">or connect with</p>
+            <p className="uppercase text-violet font-display mt-4">or connect with</p>
             <button className="gap-2 items-center justify-center border mr-3 px-4 py-2 inline-flex text-blue border-grey ">
                <Discord  /> <span className='text-white'>discord</span>
             </button>
                 
-            <button className="gap-2 items-center justify-center border mr-3 px-4 py-2 inline-flex ">
-                <Github /> <span className='text-grey'>github</span>
+            <button className="gap-2 items-center justify-center border mr-3 px-4 py-2 inline-flex border-grey text-white">
+                <Github /> <span className='text-white'>github</span>
             </button>
-            <button className=" gap-2 items-center justify-center border mr-3 px-4 py-2 inline-flex  ">
-               <Google /> google
+            <button className=" gap-2 items-center justify-center border mr-3 px-4 py-2 inline-flex text-green border-grey ">
+               <Google /> <span className='text-white'>google</span>
             </button>
  
 
             <div>
-            <button  className=" justify-center uppercase mx-auto flex w-full border  py-2 bg-green mt-5 text-center">
+            <button  className=" justify-center uppercase mx-auto flex w-full  py-2 bg-green mt-5 text-center font-display">
                 Sign in
             </button>
             </div>
             </form> 
      
        <div className="flex gap-5">
-       <p>Forgot password</p>
-       <p>New player? Create account &gt; </p>
+       <p className='text-pink'>Forgot password</p>
+       <p className='text-violet uppercase'>New player? Create account &gt; </p>
         </div>
-        
+    
+    </div>
        
     </body>
 
